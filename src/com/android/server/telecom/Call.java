376ae00c6b363f116e51040fcfdbd79e7f050e5a
Akhil Narang <akhilnarang.1999@gmail.com>
2016-04-19 20:30:26 +0530

    Revert "Proper supplementary service notification handling (3/5)"
    This reverts commit c4e4293a0093bb5e7aa84cf6738434be8765ce11.

--- src/com/android/server/telecom/Call.java
+++ src/com/android/server/telecom/Call.java
@@ -80,7 +80,6 @@ public class Call implements CreateConnectionResponse {
         void onPostDialWait(Call call, String remaining);
         void onPostDialChar(Call call, char nextChar);
         void onConnectionCapabilitiesChanged(Call call);
-        void onConnectionPropertiesChanged(Call call);
         void onParentChanged(Call call);
         void onChildrenChanged(Call call);
         void onCannedSmsResponsesLoaded(Call call);
@@ -120,8 +119,6 @@ public class Call implements CreateConnectionResponse {
         public void onPostDialChar(Call call, char nextChar) {}
         @Override
         public void onConnectionCapabilitiesChanged(Call call) {}
-        @Override
-        public void onConnectionPropertiesChanged(Call call) {}
         @Override
         public void onParentChanged(Call call) {}
         @Override
@@ -299,7 +296,6 @@ public class Call implements CreateConnectionResponse {
     private boolean mDirectToVoicemailQueryPending;
 
     private int mConnectionCapabilities;
-    private int mConnectionProperties;
 
     private boolean mIsConference = false;
 
@@ -444,7 +440,7 @@ public class Call implements CreateConnectionResponse {
         }
 
         return String.format(Locale.US,
-                "[%s, %s, %s, %s, %s, childs(%d), has_parent(%b), [%s], [%s], %b, %s]",
+                "[%s, %s, %s, %s, %s, childs(%d), has_parent(%b), [%s], %b, %s]",
                 System.identityHashCode(this),
                 CallState.toString(mState),
                 component,
@@ -453,7 +449,6 @@ public class Call implements CreateConnectionResponse {
                 getChildCalls().size(),
                 getParentCall() != null,
                 Connection.capabilitiesToString(getConnectionCapabilities()),
-                Connection.propertiesToString(getConnectionProperties()),
                 mIsActiveSub, mTargetPhoneAccountHandle);
     }
 
@@ -828,21 +823,6 @@ public class Call implements CreateConnectionResponse {
         }
     }
 
-    int getConnectionProperties() {
-        return mConnectionProperties;
-    }
-
-    void setConnectionProperties(int connectionProperties) {
-        Log.v(this, "setConnectionProperties: %s",
-                Connection.propertiesToString(connectionProperties));
-        if (mConnectionProperties != connectionProperties) {
-            mConnectionProperties = connectionProperties;
-            for (Listener l : mListeners) {
-                l.onConnectionPropertiesChanged(this);
-            }
-        }
-    }
-
     Call getParentCall() {
         return mParentCall;
     }
@@ -946,7 +926,6 @@ public class Call implements CreateConnectionResponse {
         setCallerDisplayName(
                 connection.getCallerDisplayName(), connection.getCallerDisplayNamePresentation());
         setConnectionCapabilities(connection.getConnectionCapabilities());
-        setConnectionProperties(connection.getConnectionProperties());
         setVideoProvider(connection.getVideoProvider());
         setVideoState(connection.getVideoState());
         setRingbackRequested(connection.isRingbackRequested());
